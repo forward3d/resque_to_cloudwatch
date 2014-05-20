3a60190ce57e585d719435be1ab2514694a62b4f
Andy Sykes <github@tinycat.co.uk>
2014-05-20 10:33:26 +0100

    Add work remaining metric (queue length + workers working)

--- lib/resque_to_cloudwatch/collectors.rb
+++ lib/resque_to_cloudwatch/collectors.rb
@@ -66,4 +66,31 @@ module ResqueToCloudwatch
     
   end
   
+  class WorkRemainingCollector
+    
+    def initialize(config)
+      @config = config
+    end
+    
+    def get_value
+      redis = Redis.new(:host => @config.redis_host, :port => @config.redis_port)
+      working = redis.smembers('resque:workers').select do |worker_key|
+        redis.exists("resque:worker:#{worker_key}")
+      end.length
+      queue_length = redis.smembers('resque:queues').map do |queue_key|
+        redis.llen("resque:queue:#{queue_key}")
+      end.reduce(:+)
+      working + queue_length
+    end
+    
+    def metric_name
+      "resque_work_remaining"
+    end
+    
+    def to_s
+      metric_name
+    end
+    
+  end
+  
 end
